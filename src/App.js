428b1f9266c3d55fb1ead88e5bb592849c07c68d
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-11-26 00:04:16 +0900

    feat : 라우터 생성 및 Detail 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import { useState } from "react";
 import data from "./data.js";
 import Shoe from "./Shoe.js";
+import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./Detail.js";
 
 function App() {
     let [shoes] = useState(data);
@@ -19,27 +19,36 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Cart</Nav.Link>
+                        <Link to="/">홈</Link>
+                        <Link to="/detail">상세페이지</Link>
                     </Nav>
                 </Container>
             </Navbar>
 
-            <div className="main-bg"></div>
-
-            <Container>
-                <Row>
-                    {shoes.map((s, idx) => {
-                        return (
-                            <Shoe
-                                idx={idx + 1}
-                                name={s.title}
-                                description={s.content}
-                            ></Shoe>
-                        );
-                    })}
-                </Row>
-            </Container>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <div className="main-bg"></div>
+                            <Container>
+                                <Row>
+                                    {shoes.map((s, idx) => {
+                                        return (
+                                            <Shoe
+                                                idx={idx + 1}
+                                                name={s.title}
+                                                description={s.content}
+                                            ></Shoe>
+                                        );
+                                    })}
+                                </Row>
+                            </Container>
+                        </>
+                    }
+                />
+                <Route path="/detail" element={<Detail></Detail>} />
+            </Routes>
         </div>
     );
 }
